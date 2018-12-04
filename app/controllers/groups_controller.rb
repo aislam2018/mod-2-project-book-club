374e69967e536d5b98dc428cdf9ee09ae17f6b79
Anik Islam <Anik1606@gmail.com>
2018-12-04 18:06:17 -0500

    users and groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,36 @@
 class GroupsController < ApplicationController
+  before_action :find_group, only:[:show, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
   end
 
-end
+  def new
+    @group = Group.new
+  end
+  def show
+  end
+  def edit
+  end
+  def create
+    @group = Group.create(group_params)
+    redirect_to @group
+  end
+
+  def update
+    @group.update(group_params)
+    redirect_to @group
+  end
+  def destroy
+    @group.destroy
+    redirect_to groups_path
+  end
+
+  private
+  def find_group
+    @group = Group.find(params[:id])
+  end
+  def group_params
+    params.require(:group).permit(:name, :book_id, :creator_id, :first_meeting, :days, :reading_amount)
+  end
+  end
